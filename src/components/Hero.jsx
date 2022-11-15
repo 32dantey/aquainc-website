d283b3c4bcf20b1878a86ab7834cafa83501b954
Ashay Ramolia <Ashayramolia@gmail.com>
2022-11-15 13:17:42 +0530

    Changed Image and edited hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,22 +21,31 @@ function Hero() {
     //     </div>
         
     // </section>
-    <section class="bg-gtob dark:bg-gray-900 font-poppins">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-            <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    <section id='home' className='bg-gtob font-poppins'> 
+        <div className='font-poppins'>
+        <div className="grid max-w-screen-xl px-6 py-8  lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 backdrop-blur bg-white/50">
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className=" mb-4 text-4xl  tracking-tight leading-none text-center  ">
+
+                    <span className="text-aqua-green">Aqua</span>
+                    <span> </span> 
+                    <span className="text-aqua-blue">Inc</span> 
+                    
+		</h1>
+            
+            <p className="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl">Reliable and quality products from the best there is.</p>
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-aqua-blue hover:bg-aqua-green focus:ring-4 focus:ring-primary-300 ">
+                Our History
+                <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
+            <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-200 focus:ring-4 focus:ring-gray-500 ">
+                Products
             </a> 
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"/>
+        <div className=" lg:mt-0 lg:col-span-5 lg:flex">
+            <img src={tubeSettler} alt="tubesettlermedia"/>
         </div>                
+    </div>
     </div>
     </section>
   )
